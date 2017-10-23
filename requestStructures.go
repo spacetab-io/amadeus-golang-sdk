546bc1f747e84543eb458a61c81877cfcfccc4cd
kidem <kirill.writehere@gmail.com>
2017-10-23 16:35:02 +0300

    added new request structures

--- requestStructures.go
+++ requestStructures.go
@@ -92,3 +92,54 @@ type CommandCrypticReply struct {
 		TextStringDetails string `xml:"textStringDetails,omitempty"`
 	} `xml:"longTextString,omitempty"`
 }
+
+type QueueList struct {
+	XMLName xml.Name `xml:"http://xml.amadeus.com/QDQLRQ_11_1_1A Queue_List"`
+
+	// presence implies that this is a follow up scrolling entry to a previous entry. Absence implies start of a new search
+	Scroll *ActionDetailsTypeI `xml:"scroll,omitempty"`
+
+	// used to specify the target office for which the queue count is to be displayed
+	TargetOffice *AdditionalBusinessSourceInformationTypeI `xml:"targetOffice,omitempty"`
+
+	// used to specify the queue if required
+	QueueNumber *QueueInformationType `xml:"queueNumber,omitempty"`
+
+	// used to select the category
+	CategoryDetails *SubQueueInformationType `xml:"categoryDetails,omitempty"`
+
+	// date range as system defined
+	Date *StructuredDateTimeInformationType `xml:"date,omitempty"`
+
+	// defines the start point for the search and may also define the end point of the search
+	ScanRange *RangeDetailsTypeI `xml:"scanRange,omitempty"`
+
+	SearchCriteria *SearchCriteriaType `xml:"searchCriteria,omitempty"`
+
+	// Passenger name list (all the names in the PNR).
+	PassengerName *TravellerInformationType `xml:"passengerName,omitempty"`
+
+	// The last 2 characters of the sine of the agent who placed the PNR on queue.
+	AgentSine *UserIdentificationTypeQL `xml:"agentSine,omitempty"`
+
+	// Account number issue from AIAN entry in the PNR.
+	AccountNumber *AccountingInformationElementType `xml:"accountNumber,omitempty"`
+
+	FlightInformation *FacilityInformationType `xml:"flightInformation,omitempty"`
+
+	// This is the point of sale of segments in PNRs: - 9 char Amadeus Office ID. - OR 2 char GDS code for OA PNRs  PNRs containing a segment sold in any Amadeus Office ID matching pattern NCE6X*** or ***BA0*** or sold in Sabre (1S) or Gallileo (1G).
+	Pos *PointOfSaleInformationType `xml:"pos,omitempty"`
+
+	// The repetition is 10 because we can transport: - until 5 tierLevel - until 5 customerValue, including possibly range of customerValue.  If we have tierLevel in the FTI, the customerValue must not be present. If we have customerValue in the FTI, the tierLevel must not be present.
+	TierLevelAndCustomerValue *FrequentTravellerIdentificationCodeType `xml:"tierLevelAndCustomerValue,omitempty"`
+
+	SortCriteria *SortCriteriaType `xml:"sortCriteria,omitempty"`
+}
+
+type QueueListReply struct {
+	XMLName xml.Name `xml:"http://xml.amadeus.com/QDQLRR_11_1_1A Queue_ListReply"`
+
+	ErrorReturn *ErrorReturnType `xml:"errorReturn,omitempty"`
+
+	QueueView *QueueViewType `xml:"queueView,omitempty"`
+}

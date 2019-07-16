cfcb2c0a32b1b25d80abd59a1f1bbaf577819045
alex.kadyrov <alex.kadirov@gmail.com>
2019-07-16 17:48:54 +0300

    правки по дискашену

--- client/information.go
+++ client/information.go
@@ -5,20 +5,20 @@ import (
 	"github.com/tmconsulting/amadeus-golang-sdk/structs/fare/checkRules/v07.1/response"
 	"github.com/tmconsulting/amadeus-golang-sdk/structs/fare/informativePricingWithoutPNR/v12.4/request"
 	"github.com/tmconsulting/amadeus-golang-sdk/structs/fare/informativePricingWithoutPNR/v12.4/response"
-	PNR_Reply_v11_3_request "github.com/tmconsulting/amadeus-golang-sdk/structs/pnr/retrieve/v11.3/request"
-	PNR_Reply_v11_3_response "github.com/tmconsulting/amadeus-golang-sdk/structs/pnr/retrieve/v11.3/response"
-	PNR_Reply_v19_1_request "github.com/tmconsulting/amadeus-golang-sdk/structs/pnr/retrieve/v19.1/request"
-	PNR_Reply_v19_1_response "github.com/tmconsulting/amadeus-golang-sdk/structs/pnr/retrieve/v19.1/response"
+	"github.com/tmconsulting/amadeus-golang-sdk/structs/pnr/retrieve/v11.3/request"
+	"github.com/tmconsulting/amadeus-golang-sdk/structs/pnr/retrieve/v11.3/response"
+	"github.com/tmconsulting/amadeus-golang-sdk/structs/pnr/retrieve/v19.1/request"
+	"github.com/tmconsulting/amadeus-golang-sdk/structs/pnr/retrieve/v19.1/response"
 )
 
-func (client *AmadeusClient) PNRRetrieveV113(query *PNR_Reply_v11_3_request.Request) (*PNR_Reply_v11_3_response.Response, *ResponseSOAPHeader, error) {
-	var reply PNR_Reply_v11_3_response.Response
+func (client *AmadeusClient) PNRRetrieveV113(query *PNR_Retrieve_v11_3_request.Request) (*PNR_Retrieve_v11_3_response.Response, *ResponseSOAPHeader, error) {
+	var reply PNR_Retrieve_v11_3_response.Response
 	header, err := client.service.Call(soapUrl, "PNRRET_11_3_1A", query, &reply, client)
 	return &reply, header, err
 }
 
-func (client *AmadeusClient) PNRRetrieveV191(query *PNR_Reply_v19_1_request.Request) (*PNR_Reply_v19_1_response.Response, *ResponseSOAPHeader, error) {
-	var reply PNR_Reply_v19_1_response.Response
+func (client *AmadeusClient) PNRRetrieveV191(query *PNR_Retrieve_v19_1_request.Request) (*PNR_Retrieve_v19_1_response.Response, *ResponseSOAPHeader, error) {
+	var reply PNR_Retrieve_v19_1_response.Response
 	header, err := client.service.Call(soapUrl, "PNRRET_19_1_1A", query, &reply, client)
 	return &reply, header, err
 }

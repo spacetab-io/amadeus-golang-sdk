e8855a86b715b64f47846100aeae37a6143d5312
alex.kadyrov <alex.kadirov@gmail.com>
2019-08-09 12:47:40 +0300

    CalculateVAT

--- sdk/vatCalculator/CalculateVAT/query.go
+++ sdk/vatCalculator/CalculateVAT/query.go
@@ -1,7 +1,10 @@
 package calculatevat
 
+import "encoding/xml"
+
 // CalculateVAT was auto-generated from WSDL.
 type CalculateVAT struct {
-	PINCode  *string `xml:"PINCode,omitempty" json:"PINCode,omitempty" yaml:"PINCode,omitempty"`
-	TSTMasks *string `xml:"TSTMasks,omitempty" json:"TSTMasks,omitempty" yaml:"TSTMasks,omitempty"`
+	XMLName  xml.Name `xml:"http://amadeus.ru/CalculateVAT CalculateVAT"`
+	PINCode  *string  `xml:"PINCode,omitempty" json:"PINCode,omitempty" yaml:"PINCode,omitempty"`
+	TSTMasks *string  `xml:"TSTMasks,omitempty" json:"TSTMasks,omitempty" yaml:"TSTMasks,omitempty"`
 }

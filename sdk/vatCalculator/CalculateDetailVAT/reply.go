6db24f38362e294dacfb5dd4644975c76a94155f
alex.kadyrov <alex.kadirov@gmail.com>
2019-08-13 17:32:28 +0300

    CalculateDetailVAT changes

--- sdk/vatCalculator/CalculateDetailVAT/reply.go
+++ sdk/vatCalculator/CalculateDetailVAT/reply.go
@@ -7,16 +7,24 @@ type CalculateDetailVATResponse struct {
 
 // VATDetailCalculationResult was auto-generated from WSDL.
 type VATDetailCalculationResult struct {
-	VATDetailCalculationData []*TSTVATDetailCalculationResult `xml:"VATDetailCalculationData,omitempty" json:"VATDetailCalculationData,omitempty" yaml:"VATDetailCalculationData,omitempty"`
-	VATCalculationMessage    *string                          `xml:"VATCalculationMessage,omitempty" json:"VATCalculationMessage,omitempty" yaml:"VATCalculationMessage,omitempty"`
+	VATDetailCalculationData []*TSTVATDetailCalculationResults `xml:"VATDetailCalculationData,omitempty" json:"VATDetailCalculationData,omitempty" yaml:"VATDetailCalculationData,omitempty"`
+	VATCalculationMessage    *string                           `xml:"VATCalculationMessage,omitempty" json:"VATCalculationMessage,omitempty" yaml:"VATCalculationMessage,omitempty"`
+}
+
+type TSTVATDetailCalculationResults struct {
+	TSTVATDetailCalculationResult TSTVATDetailCalculationResult `xml:"TSTVATDetailCalculationResult,omitempty" json:"TSTVATDetailCalculationResult,omitempty" yaml:"TSTVATDetailCalculationResult,omitempty"`
 }
 
 // TSTVATDetailCalculationResult was auto-generated from WSDL.
 type TSTVATDetailCalculationResult struct {
-	VATTSTsID                  *string                           `xml:"VATTSTsID,omitempty" json:"VATTSTsID,omitempty" yaml:"VATTSTsID,omitempty"`
-	VATPAXexID                 *string                           `xml:"VATPAXexID,omitempty" json:"VATPAXexID,omitempty" yaml:"VATPAXexID,omitempty"`
-	VATSegmentsID              *string                           `xml:"VATSegmentsID,omitempty" json:"VATSegmentsID,omitempty" yaml:"VATSegmentsID,omitempty"`
-	VATDetailCalculationResult []*VATItemDetailCalculationResult `xml:"VATDetailCalculationResult,omitempty" json:"VATDetailCalculationResult,omitempty" yaml:"VATDetailCalculationResult,omitempty"`
+	VATTSTsID                  *string                            `xml:"VATTSTsID,omitempty" json:"VATTSTsID,omitempty" yaml:"VATTSTsID,omitempty"`
+	VATPAXexID                 *string                            `xml:"VATPAXexID,omitempty" json:"VATPAXexID,omitempty" yaml:"VATPAXexID,omitempty"`
+	VATSegmentsID              *string                            `xml:"VATSegmentsID,omitempty" json:"VATSegmentsID,omitempty" yaml:"VATSegmentsID,omitempty"`
+	VATDetailCalculationResult []*VATItemDetailCalculationResults `xml:"VATDetailCalculationResult,omitempty" json:"VATDetailCalculationResult,omitempty" yaml:"VATDetailCalculationResult,omitempty"`
+}
+
+type VATItemDetailCalculationResults struct {
+	VATItemDetailCalculationResult VATItemDetailCalculationResult `xml:"VATItemDetailCalculationResult,omitempty" json:"VATItemDetailCalculationResult,omitempty" yaml:"VATItemDetailCalculationResult,omitempty"`
 }
 
 // VATItemDetailCalculationResult was auto-generated from WSDL.

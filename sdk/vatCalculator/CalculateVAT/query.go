900483e546c94c2384c902b3a6b042a24589f5d9
alex.kadyrov <alex.kadirov@gmail.com>
2019-08-13 13:15:53 +0300

    struct fix

--- sdk/vatCalculator/CalculateVAT/query.go
+++ sdk/vatCalculator/CalculateVAT/query.go
@@ -4,7 +4,7 @@ import "encoding/xml"
 
 // CalculateVAT was auto-generated from WSDL.
 type CalculateVAT struct {
-	XMLName  xml.Name `xml:"http://amadeus.ru/CalculateVAT CalculateVAT"`
+	XMLName  xml.Name `xml:"http://amadeus.ru/ CalculateVAT"`
 	PINCode  *string  `xml:"PINCode,omitempty" json:"PINCode,omitempty" yaml:"PINCode,omitempty"`
 	TSTMasks *string  `xml:"TSTMasks,omitempty" json:"TSTMasks,omitempty" yaml:"TSTMasks,omitempty"`
 }
